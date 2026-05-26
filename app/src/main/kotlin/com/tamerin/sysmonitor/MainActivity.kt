71413780f387b155f72542f0491d1da99e99d217
theredstonee <blitz.der.wahrheit@gmail.com>
2026-05-26 19:04:47 +0200

    Fix missing setValue import for state delegation in MainActivity

--- app/src/main/kotlin/com/tamerin/sysmonitor/MainActivity.kt
+++ app/src/main/kotlin/com/tamerin/sysmonitor/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
